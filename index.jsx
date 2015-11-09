af659f54ad6a981b63880817dafacb7ed9924934
caasi Huang <caasi@users.noreply.github.com>
2015-11-09 17:28:02 +0800

    Setup Redux

--- index.jsx
+++ index.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './lib/containers/App';
+import configureStore from './lib/store/configureStore';
 
 import parse from './lib/parse';
 
+const store = configureStore();
+
 render(
-  <Provider>
-    <App name={parse()[1]} />
+  <Provider store={store}>
+    <App />
   </Provider>,
   document.getElementById('app')
 );
--- lib/containers/App/index.jsx
+++ lib/containers/App/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as FoobarActions from '../../actions/foobar';
 
-export default props => (
-  <div className="index">
-    hello, {props.name}
-  </div>
+function mapStateToProps(state) {
+  return { foobar: state.foobar };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(FoobarActions, dispatch) }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  (props) => {
+    let { foobar, actions } = props;
+    return (
+      <div className="index" onClick={actions.foobar}>
+        {foobar ? 'hi' : 'hello'}
+      </div>
+    );
+  }
 );
