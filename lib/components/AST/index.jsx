6e2b2e2b0d456a8adcd694dd24aa94fd3c788138
caasi Huang <caasi@users.noreply.github.com>
2015-11-10 12:55:32 -0800

    Style the outputs

--- lib/components/AST/index.jsx
+++ lib/components/AST/index.jsx
@@ -2,11 +2,47 @@ import React from 'react';
 
 import styles from './index.css';
 
+const Var = props => (
+  <div className="term variable">
+    <div>["var", "{props.name}"]</div>
+  </div>
+);
+
+const Lam = props => (
+  <div className="term abstraction">
+    <div>["lam", "{props.name}",</div>
+    {props.children}
+    <div>]</div>
+  </div>
+);
+
+const App = props => (
+  <div className="term application">
+    <div>["app",</div>
+    {props.children}
+    <div>]</div>
+  </div>
+);
+
+function construct(tree) {
+  switch (tree[0]) {
+    case 'var':
+      return <Var name={tree[1]}/>;
+    case 'lam':
+      return <Lam name={tree[1]}>{construct(tree[2])}</Lam>;
+    case 'app':
+      return <App>
+        {construct(tree[1])}
+        {construct(tree[2])}
+      </App>
+  }
+}
+
 export default props => {
   let { className, children, tree } = props;
   return (
     <div className={`${styles.className} ${className}`}>
-      {tree ? JSON.stringify(tree, null, 2) : null}
+      {tree ? construct(tree) : null}
     </div>
   );
 };
