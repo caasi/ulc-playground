19b59f1c567efdfeaa1d70c69874c23818c5b16a
caasi Huang <caasi.igd@gmail.com>
2016-06-06 03:29:10 +0800

    Try to understand (->)

--- index.jsx
+++ index.jsx
@@ -4,6 +4,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './lib/containers/App';
 import configureStore from './lib/store/configureStore';
+import './lib/utils/lens'
+import './lib/utils/function'
 
 import 'normalize.css/normalize.css';
 import './index.css';
